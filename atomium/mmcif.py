16e8c8f104b3359c217230f80100df8391d60d4f
Bastian Cornelsen <kontakt@bastiancornelsen.de>
2020-11-22 19:36:48 +0100

    Added _entity_poly.type of mmcif files to chains

--- atomium/mmcif.py
+++ atomium/mmcif.py
@@ -429,7 +429,11 @@ def update_models_list(mmcif_dict, data_dict):
             model_num = atom["pdbx_PDB_model_num"]
         mol_type = types[entities[atom["label_asym_id"]]]
         if mol_type == "polymer":
-            poly_type = [ el for el in mmcif_dict['entity_poly'] if el['entity_id'] == model_num][0]['type'] if 'entity_poly' in mmcif_dict else None
+            poly_type = [ el for el in mmcif_dict['entity_poly'] if el['entity_id'] == model_num]
+            if len(poly_type) > 0:
+                poly_type = [ el for el in mmcif_dict['entity_poly'] if el['entity_id'] == model_num][0]['type']
+            else:
+                poly_type = None
             add_polymer_to_polymer(atom, aniso, model, names, poly_type)
         elif mol_type == "branched":
             add_atom_to_polymer(atom, aniso, model, names)
